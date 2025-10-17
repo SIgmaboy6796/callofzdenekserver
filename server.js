a0b96ab64cfd4b3fd687fce0a089e63e557b5078
SIgmaboy6796 <ohioemail7@gmail.com>
2025-10-17 13:38:53 +0200

    Log player names when they join the game

--- server.js
+++ server.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+socket.on('playerJoined', function (name) {
+    console.log (name);
+});
